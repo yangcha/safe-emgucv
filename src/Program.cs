43a05cb03bf5bcc8801b5977d5b2d48962c8bd37
Changjiang Yang <changjiangyang@noahmed.com>
2023-09-01 19:10:29 -0400

    Make disposable reference to Image

--- src/Program.cs
+++ src/Program.cs
@@ -2,10 +2,24 @@
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using System.Runtime.InteropServices;
+
 
 using Image8u = CV.Image<byte>;
+using Image8s = CV.Image<sbyte>;
+using Image16u = CV.Image<ushort>;
+using Image16s = CV.Image<short>;
+using Image32s = CV.Image<int>;
 using Image32f = CV.Image<float>;
 using Image64f = CV.Image<double>;
+
+using Image8uAsMat = CV.Image8uAsMat;
+using Image8sAsMat = CV.Image8sAsMat;
+using Image16uAsMat = CV.Image16uAsMat;
+using Image16sAsMat = CV.Image16sAsMat;
+using Image32sAsMat = CV.Image32sAsMat;
+using Image32fAsMat = CV.Image32fAsMat;
+using Image64fAsMat = CV.Image64fAsMat;
 
 namespace CV
 {
@@ -24,6 +38,123 @@ namespace CV
             Channels = channels;
         }
     }
+
+    public class ImageAsMat<T> : IDisposable
+    {
+        protected GCHandle handle;
+        public Mat Mat { get; set; }
+        private bool disposed = false;
+
+        protected ImageAsMat()
+        {
+            // Prevent usage of default constructor
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    Mat.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                handle.Free();
+                disposed = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~ImageAsMat()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        void IDisposable.Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            // This object will be cleaned up by the Dispose method.
+            // Therefore, you should call GC.SuppressFinalize to
+            // take this object off the finalization queue
+            // and prevent finalization code for this object
+            // from executing a second time.
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public class Image8uAsMat : ImageAsMat<byte>
+    {
+        // The class constructor.
+        public Image8uAsMat(Image8u image)
+        {
+            this.handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            Mat = new Mat(image.Height, image.Width, DepthType.Cv8U, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels);
+        }
+    }
+
+    public class Image8sAsMat : ImageAsMat<sbyte>
+    {
+        // The class constructor.
+        public Image8sAsMat(Image8s image)
+        {
+            this.handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            Mat = new Mat(image.Height, image.Width, DepthType.Cv8S, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels);
+        }
+    }
+
+    public class Image16uAsMat : ImageAsMat<ushort>
+    {
+        // The class constructor.
+        public Image16uAsMat(Image16u image)
+        {
+            this.handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            Mat = new Mat(image.Height, image.Width, DepthType.Cv16U, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(ushort));
+        }
+    }
+
+    public class Image16sAsMat : ImageAsMat<short>
+    {
+        // The class constructor.
+        public Image16sAsMat(Image16s image)
+        {
+            this.handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            Mat = new Mat(image.Height, image.Width, DepthType.Cv16S, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(short));
+        }
+    }
+
+    public class Image32sAsMat : ImageAsMat<int>
+    {
+        // The class constructor.
+        public Image32sAsMat(Image32s image)
+        {
+            this.handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            Mat = new Mat(image.Height, image.Width, DepthType.Cv32S, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(int));
+        }
+    }
+
+    public class Image32fAsMat : ImageAsMat<float>
+    {
+        // The class constructor.
+        public Image32fAsMat(Image32f image)
+        {
+            this.handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            Mat = new Mat(image.Height, image.Width, DepthType.Cv32F, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(float));
+        }
+    }
+
+    public class Image64fAsMat : ImageAsMat<double>
+    {
+        // The class constructor.
+        public Image64fAsMat(Image64f image)
+        {
+            this.handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            Mat = new Mat(image.Height, image.Width, DepthType.Cv64F, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(double));
+        }
+    }
 }
 
 namespace emgucv_example
@@ -33,8 +164,12 @@ namespace emgucv_example
         static void Main(string[] args)
         {
            
-            var bimg = new Image8u(400, 200, 3); 
-            String win1 = "Test Window"; //The name of the window
+            var bimg = new Image8u(400, 200, 3);
+            using (var m = new CV.Image8uAsMat(bimg))
+            {
+                CvInvoke.BitwiseNot(m.Mat, m.Mat);
+            }
+                String win1 = "Test Window"; //The name of the window
             CvInvoke.NamedWindow(win1); //Create the window using the specific name
 
             unsafe
