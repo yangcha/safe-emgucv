06b3fbb1b8ab3f6c77657a4180afb7cad49bd6b8
Changjiang Yang <yangcj@gmail.com>
2023-09-04 15:11:57 -0400

    simplify code

--- src/Managed.ImageAsMat.cs
+++ src/Managed.ImageAsMat.cs
@@ -22,45 +22,43 @@ namespace Managed
         public Mat Mat { get; set; }
         private bool disposed = false;
 
-        public ImageAsMat(Image8u image)
+        private ImageAsMat(Object data)
         {
-            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            handle = GCHandle.Alloc(data, GCHandleType.Pinned);
+        }
+
+        public ImageAsMat(Image8u image) : this(image.Data)
+        {
             Mat = new Mat(image.Height, image.Width, DepthType.Cv8U, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels);
         }
 
-        public ImageAsMat(Image8s image)
+        public ImageAsMat(Image8s image) : this(image.Data)
         {
-            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
             Mat = new Mat(image.Height, image.Width, DepthType.Cv8S, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels);
         }
 
-        public ImageAsMat(Image16u image)
+        public ImageAsMat(Image16u image) : this(image.Data)
         {
-            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
             Mat = new Mat(image.Height, image.Width, DepthType.Cv16U, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(ushort));
         }
 
-        public ImageAsMat(Image16s image)
+        public ImageAsMat(Image16s image) : this(image.Data)
         {
-            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
             Mat = new Mat(image.Height, image.Width, DepthType.Cv16S, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(short));
         }
 
-        public ImageAsMat(Image32s image)
+        public ImageAsMat(Image32s image) : this(image.Data)
         {
-            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
             Mat = new Mat(image.Height, image.Width, DepthType.Cv32S, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(int));
         }
 
-        public ImageAsMat(Image32f image)
+        public ImageAsMat(Image32f image) : this(image.Data)
         {
-            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
             Mat = new Mat(image.Height, image.Width, DepthType.Cv32F, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(float));
         }
 
-        public ImageAsMat(Image64f image)
+        public ImageAsMat(Image64f image) : this(image.Data)
         {
-            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
             Mat = new Mat(image.Height, image.Width, DepthType.Cv64F, image.Channels, handle.AddrOfPinnedObject(), image.Width * image.Channels * sizeof(double));
         }
 
